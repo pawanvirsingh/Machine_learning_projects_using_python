6a7316ea9627ccebf18dc5260b414bd6baa73aec
Pawanvir singh <pawanvir.singh26@gmail.com>
2017-11-20 00:28:41 +0530

    plotting of data for train and test using matplot lib

--- Part 2 - Regression/Section 4 - Simple Linear Regression/selfsimple_linear_regression.py
+++ Part 2 - Regression/Section 4 - Simple Linear Regression/selfsimple_linear_regression.py
@@ -11,10 +11,10 @@ import matplotlib.pyplot as plt
 import pandas as pd 
 
 dataset= pd.read_csv("Salary_Data.csv")
-X = dataset.iloc[:, :-1].values
+X =dataset.iloc[:, :-1].values
 y = dataset.iloc[:, 1].values
 
-#data preprocessing step this is 
+#data prep rocessing step this is 
 
 from sklearn.cross_validation  import train_test_split
 X_train,x_test, y_train,y_test = train_test_split(X,y,test_size=1/3,random_state=0)
@@ -24,5 +24,27 @@ X_train,x_test, y_train,y_test = train_test_split(X,y,test_size=1/3,random_state
 from sklearn.linear_model import LinearRegression
 regressor=LinearRegression()
 regressor.fit(X_train,y_train)
+y_predict = regressor.predict(x_test)
 
-y_predict = regressor.predict(X_test)
+"""
+error = y_test-y_predict
+e= np.sum(error)
+abserror = np.absolute(error)
+sqrarr= np.square(error)
+addall= np.sum(sqrarr)
+"""
+#for ploting the data  for train data
+plt.scatter(X_train,y_train,color='red')
+plt.plot(X_train,regressor.predict(X_train),color='blue')
+plt.title("Salary Vs Experience (train)")
+plt.xlabel("Experience")
+plt.ylabel("Salary")
+plt.show()
+
+#for ploting the data  for test data
+plt.scatter(x_test,y_test,color='red')
+plt.plot(x_test,regressor.predict(x_test),color='blue')
+plt.title("Salary Vs Experience (Test data set)")
+plt.xlabel("Experience")
+plt.ylabel("Salary")
+plt.show()
